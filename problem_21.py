3a98e317c026e5f7e62eca1b142c8477167d229a
Dymor <teo.miscia@gmail.com>
2019-04-16 15:28:57 +0200

    Small update

--- problem_21.py
+++ problem_21.py
@@ -7,26 +7,21 @@ pp = pprint.PrettyPrinter(indent=4)
 from functools import reduce
 
 def factors(n):    
-    return set(reduce(list.__add__, 
-                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
+    return sorted(set(reduce(list.__add__, 
+                ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0))))
 
-
-
-dictnumbers = dict()
+amicables = list()
 for x in range(1,10001):
 	tot = numpy.sum(list(factors(x))[:-1])
-	if tot in dictnumbers:
-		dictnumbers[tot].append(x)
-	else:
-		dictnumbers[tot] = [x]
+
+	if tot > 1 and tot != x:
+		tot2 = numpy.sum(list(factors(tot))[:-1])
+
+		if tot2 == x:
+			amicables.append([tot,x])
 
 solution = 0
-for k,v in dictnumbers.items():
-	for vv in v:
-		if vv in dictnumbers.keys() and k in dictnumbers[vv] and vv < 10000 and k < 10000 and vv != k:
-			print(str(vv)+" - "+str(k))
-			solution = solution + vv + k
+for v in amicables:
+	solution = solution + v[0] + v[1]
 solution = solution / 2
-pp.pprint(dictnumbers[5209])
-pp.pprint(factors(5))
 print("Solution is: "+str(solution))
